57ef19d2ddec2f8a898d36f9584cea4621ee1a97
Michael Sloan <mgsloan@lappy.(none)>
2012-01-15 22:05:09 -0800

    Remove debug code

--- src/TestHandle.hs
+++ src/TestHandle.hs
@@ -6,18 +6,11 @@ import Diagrams.Prelude
 
 import Debug.Trace
 
-debug x = trace (show x) x
-
 data TestState = TestState (Draggable CairoDiagram)
 
-traceTS ts@(TestState (Draggable a b _)) = trace (show a ++ " " ++ show b) ts
-
 instance Interactive TestState where
-  mouse m i (TestState d) = (traceTS . TestState) <$> mouse m i d
-  display dw i (TestState x) = (traceTS . TestState) <$> display dw i x
+  mouse m i (TestState d) = TestState <$> mouse m i d
+  display dw i (TestState x) = TestState <$> display dw i x
   keyboard = quitKeyboard
 
-main = runToy $ mkDraggable (50, 50) handle
-
-handle :: CairoDiagram
-handle = circle 5
+main = runToy $ mkDraggable (50, 50) (circle 5 :: CairoDiagram)
