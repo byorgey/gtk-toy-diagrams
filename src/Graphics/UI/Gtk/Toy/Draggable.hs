b67d3809e6aa9ece03dc079fdb2026d913c64b33
Michael Sloan <mgsloan@lappy.(none)>
2012-01-20 23:03:39 -0800

    More generic draggable

--- src/Graphics/UI/Gtk/Toy/Draggable.hs
+++ src/Graphics/UI/Gtk/Toy/Draggable.hs
@@ -3,6 +3,7 @@
            , TypeSynonymInstances
            , FlexibleInstances
            , FlexibleContexts
+           , TypeFamilies
            , UndecidableInstances #-}
 -----------------------------------------------------------------------------
 -- |
@@ -43,7 +44,7 @@ import Diagrams.TwoD.Text
 
 -- | Clickable things have some concept of which positions are clickable.
 class Clickable a where
-  clickInside :: a -> R2 -> Bool
+  clickInside :: a -> V a -> Bool
 
 instance Clickable CairoDiagram where
   clickInside d = getAny . runQuery (query d) . P
@@ -52,32 +53,32 @@ instance Clickable CairoDiagram where
 -- | Draggable things are translatable, and store state during the drag
 --   process.
 data Draggable a = Draggable 
-  { _dragState :: (Maybe (R2, R2))
-  , _dragOffsetAcc :: R2
+  { _dragState :: Maybe (V a, V a)
+  , _dragOffsetAcc :: V a
   , _dragContent :: a
   }
 
+type instance V (Draggable a) = V a
+
 $(mkLabels [''Draggable])
 
-instance Functor Draggable where
-  fmap f (Draggable s o c) = Draggable s o $ f c
-
-instance Diagrammable a Cairo R2
-      => Diagrammable (Draggable a) Cairo R2 where
+instance ( v ~ V a, HasLinearMap v, InnerSpace v, OrderedField (Scalar v)
+         , Diagrammable a Cairo v)
+      => Diagrammable (Draggable a) Cairo v where
   toDiagram d@(Draggable _ _ a)
     = translate (dragOffset d) $ toDiagram a
 
-instance (Diagrammable a Cairo R2, Clickable a)
+instance (R2 ~ V a, Diagrammable a Cairo R2, Clickable a)
       => Interactive (Draggable a) where
   display = displayDiagram toDiagram
   mouse = simpleMouse mouseDrag
 
-instance Clickable a
+instance (Clickable a, AdditiveGroup (V a))
       => Clickable (Draggable a) where
   clickInside d p = clickInside (_dragContent d) $ p ^-^ dragOffset d
 
 -- | Creates dragging state for some object, with an initial offset.
-mkDraggable :: R2 -> a -> Draggable a
+mkDraggable :: V a -> a -> Draggable a
 mkDraggable = Draggable Nothing
 
 -- | Pure mouse handler, compatible with the type expected by "simpleMouse".
@@ -87,18 +88,19 @@ mouseDrag (Just (False, 0)) p d                   = endDrag d
 mouseDrag _ _ d = d
 
 -- | Switches into dragging mode at the given position.
-startDrag :: R2 -> Draggable a -> Draggable a
+startDrag :: V a -> Draggable a -> Draggable a
 startDrag p = set dragState (Just (p, p))
 
 -- | Updates the drag with a new mouse position, if the object is being
 --   dragged.  TODO: consider having a check for the input state to
 --   check if the mouse is being held down?
-updateDrag :: R2 -> Draggable a -> Draggable a
+updateDrag :: V a -> Draggable a -> Draggable a
 updateDrag p (Draggable (Just (_, s)) o c) = Draggable (Just (p, s)) o c
 updateDrag _ d = d
 
 -- | Switches out of dragging mode.
-endDrag :: Draggable a -> Draggable a
+endDrag :: (AdditiveGroup (V a)) 
+        => Draggable a -> Draggable a
 endDrag d = Draggable Nothing (dragOffset d) $ _dragContent d
 
 -- | Queries whether we're currently in dragging-modk
@@ -106,4 +108,6 @@ isDragging :: Draggable a -> Bool
 isDragging = isJust . get dragState
 
 -- | Gets the current amount of drag-induced offset for the diagram.
-dragOffset (Draggable c o _) =  o ^+^ (maybe (0, 0) (uncurry (^-^)) c)
+dragOffset :: (AdditiveGroup (V a))
+           => Draggable a -> V a
+dragOffset (Draggable c o _) =  o ^+^ maybe zeroV (uncurry (^-^)) c
